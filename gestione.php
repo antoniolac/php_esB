3fc8883fbecd4e541b606c079d1942e73b624b70
antoniolac <131681953+antoniolac@users.noreply.github.com>
2025-09-24 12:29:53 +0200

    esercizo B completato

---
+++ gestione.php
@@ -0,0 +1,46 @@
+<?php
+    $nome = $_GET["first_name"];
+    $cognome = $_GET["last_name"];
+    $prodotti = $_GET["prodotti"];
+    $nprodotti = $_GET["nProdotti"];
+
+    $prezzo = 0;
+    if($prodotti == "mela") {
+        $prezzo= 1;
+    }
+    elseif($prodotti == "banana") {
+        $prezzo = 2;
+    }
+    else{
+        $prezzo = 3;
+    }
+
+    //sconto 10%
+    if($nprodotti >= "10") {
+        $prezzo = $prezzo - ($prezzo * 0.10);
+    }
+
+    
+    $prezzo_senza_iva = $prezzo * $nprodotti;
+    $prezzo_con_iva = $prezzo_senza_iva * 1.22; 
+
+    
+   
+
+?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <?php
+        echo "Ciao " . $nome . " " . $cognome . "<br>";
+        echo "Hai acquistato " . $nprodotti . " " . $prodotti . "<br>";
+        echo "Prezzo totale (senza IVA): " . $prezzo_senza_iva . " €<br>";
+        echo "Prezzo totale (con IVA 22%): " . $prezzo_con_iva . " €";
+    ?>
+</body>
+</html>
